37580ccc26475bb09f4329bc5b252e28d2bc4abd
Fratge <sebastiengross@orange.fr>
2021-06-07 09:43:17 +0200

    feat: add light/dark theme

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,19 @@
 import '@fontsource/limelight/400.css'
 import '@fontsource/montserrat/400.css'
+
+const themeButton = document.getElementById('theme')
+const body = document.querySelector('body')
+
+themeButton.addEventListener('click', () => {
+  let theme = body.className
+  if (theme === 'dark') {
+    theme = 'light'
+  } else {
+    theme = 'dark'
+  }
+  body.className = theme
+  localStorage.setItem('theme', theme)
+})
+
+const theme = localStorage.getItem('theme') ?? 'dark'
+body.className = theme
