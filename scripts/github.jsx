7b0eade46a2a53d109958d737a6806c109fae6d1
Fratge <sebastiengross@orange.fr>
2021-06-08 15:01:51 +0200

    feat: stars counter api

---
+++ scripts/github.jsx
@@ -0,0 +1,31 @@
+import 'regenerator-runtime/runtime'
+import React from 'react'
+import { render } from 'react-dom'
+
+function starsCounter(stars) {
+  if (stars >= 1) {
+    return stars
+  }
+  let emojiStars = ''
+  for (let index = 0; index < stars; index++) {
+    emojiStars += '⭐'
+  }
+  return emojiStars
+}
+
+function GitHub() {
+  const [stars, setStars] = React.useState(0)
+  React.useEffect(() => {
+    async function fetchStars() {
+      const response = await fetch(
+        'https://api.github.com/repos/Fratge/website?page=1&per_page=100'
+      )
+      const data = await response.json()
+      setStars(data.stargazers_count)
+    }
+    fetchStars()
+  }, [])
+  return <p>Nombres d'étoiles : {starsCounter(stars)}</p>
+}
+
+render(<GitHub />, document.getElementById('github'))
